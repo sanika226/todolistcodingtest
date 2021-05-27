c62a46d07276fb40c4a5e6920131314ad6eac990
sanika226 <78407974+sanika226@users.noreply.github.com>
2021-05-27 16:05:06 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import NameList from './components/NameList'
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div>
+      <NameList/>
+      <TodoList/>
+    </div>
+  );
+}
+
+export default App;
